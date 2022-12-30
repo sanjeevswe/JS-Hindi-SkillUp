edf0c9b62fb4611a4e727d9693de52b040993d34
Sanjeev Kumar <75729980+sanjeevswe@users.noreply.github.com>
2022-12-30 09:54:58 +0000

    dataTypes

---
+++ 1_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all the JS code as a newer version
+//alert(3 + 3) //we are using nodejs, not browser
+
+console.log(93
+     +
+      7) //readability should be high
+
+console.log("Sanjeev")
+
+let name = "Sanjeev"
+let age = 20
+let isLoggedIn = true
+
+
+// number => 2 to power 53
+// string =>""
+// bigint
+//boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined)   //undefined
+console.log(typeof null)        //object
